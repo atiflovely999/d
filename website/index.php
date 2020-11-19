54e86f5ea221c49f10335440d14fed93aa41d421
atiflovely999 <74484457+atiflovely999@users.noreply.github.com>
2020-11-20 00:10:13 +0200

    Add files via upload

---
+++ website/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html dir="rtl">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>my page</title>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<section id="slide">
+	<div id="slideshow">
+	<ul>
+		<li><img src="img/n.jpg" alt="slide1" /></li>
+        <li><img src="img/g.jpg" alt="slide3" /></li>
+        <li><img src="img/1.png" alt="slide3" /></li>
+        <li><img src="img/o.jpg" alt="slide1" /></li>
+	</ul>
+    </div>
+    <article>ما فعله غيرك بلا شك <b>تستطيع فعله</b>, المستحيل مجرد كلمه<br/><br/>
+     
+</section>
+
+<header>
+	<section id="content">
+		<h1><strong>أسمر</strong>املتحي</h1>
+        <b>اتصل بي الآن - هاتف مجاني<span class="one"></span><span class="rest">249926999275+</span></b>
+    </section>
+</header>
+
+<nav>
+	<ul>
+		<li><a href="saura.html" title="homepage">ثوره ديسمبر</a>
+        <li><a href="hadol.html" title="whoweare">النابغه هديل أنور</a>
+        <li><a href="book.html" title="makedonations">كتب</a>
+        <li><a href="islam.html" title="ourwork">اسلاميه</a>
+        <li><a href="https://web.facebook.com/media/set/?set=a.141901729540279&type" title="contacts">فيسبوك</a>
+    </ul>
+</nav>
+
+
+<footer>
+    
+
+    </article>
+<section id="copy"></section>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+<script src="js/craftyslide.min.js"></script>
+<script>
+$("#slideshow").craftyslide();
+</script> 
+
+</body>
+</html>
